22bd4823cf98ca843a5a4fe41eca1a18e70c4916
abdullah-mamun369 <maamamun369@gmail.com>
2022-10-26 02:47:44 +0600

    Slider has added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,27 +1,38 @@
 import React from 'react';
+import 'flowbite';
 
 const Home = () => {
     return (
         <div>
             <div className="carousel w-full">
-                <div id="item1" className="carousel-item w-full">
+                <div id="slide1" className="carousel-item relative w-full">
                     <img src="https://placeimg.com/800/200/arch" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
                 </div>
-                <div id="item2" className="carousel-item w-full">
+                <div id="slide2" className="carousel-item relative w-full">
                     <img src="https://placeimg.com/800/200/arch" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
                 </div>
-                <div id="item3" className="carousel-item w-full">
+                <div id="slide3" className="carousel-item relative w-full">
                     <img src="https://placeimg.com/800/200/arch" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
                 </div>
-                <div id="item4" className="carousel-item w-full">
+                <div id="slide4" className="carousel-item relative w-full">
                     <img src="https://placeimg.com/800/200/arch" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
                 </div>
-            </div>
-            <div className="flex justify-center w-full py-2 gap-2">
-                <a href="#item1" className="btn btn-xs">1</a>
-                <a href="#item2" className="btn btn-xs">2</a>
-                <a href="#item3" className="btn btn-xs">3</a>
-
             </div>
         </div>
     );
--- src/routes/routes/Routes.js
+++ src/routes/routes/Routes.js
@@ -1,6 +1,9 @@
 import Main from "../../layout/Main";
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../../Components/Home/Home";
+import Courses from "../../Components/Courses/Courses"
+import Course from "../../Components/Course/Course"
+import Faq from "../../Components/FAQ/Faq"
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +13,21 @@ export const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home></Home>
+            },
+
+            {
+                path: "/courses",
+                element: <Courses></Courses>
+            },
+
+            {
+                path: "/course",
+                element: <Course></Course>
+            },
+
+            {
+                path: "/faq",
+                element: <Faq></Faq>
             }
         ]
     }
