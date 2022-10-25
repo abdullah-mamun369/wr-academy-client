22b390e3b244adf68a4c60ef861d7756af9765c3
abdullah-mamun369 <maamamun369@gmail.com>
2022-10-26 00:34:27 +0600

    Firebase has been added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC107wAM5237dxTzw4dTZDa2n3_BOp1RPA",
+    authDomain: "assignment-ph-10.firebaseapp.com",
+    projectId: "assignment-ph-10",
+    storageBucket: "assignment-ph-10.appspot.com",
+    messagingSenderId: "188014577468",
+    appId: "1:188014577468:web:dce30982e74e87eb4e8039"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
