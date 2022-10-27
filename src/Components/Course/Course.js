88fe1e7b0eb03976bae05daee608da5c0843e4a8
abdullah-mamun369 <maamamun369@gmail.com>
2022-10-27 20:56:40 +0600

    React to pdf has created

--- src/Components/Course/Course.js
+++ src/Components/Course/Course.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import ReactPrint from 'react-to-print';
+import { useRef } from 'react';
 
 const Course = () => {
     const courseInfo = useLoaderData();
-    console.log(courseInfo);
+    // console.log(courseInfo);
     const { image, title, mentor, classes, seats, duration, price, time, date } = courseInfo;
+    const ref = useRef();
+
+
 
     return (
-        <div className="card lg:card-side bg-base-100 shadow-xl m-5 border">
+        <div ref={ref} className="card lg:card-side bg-base-100 shadow-xl m-5 border">
             <figure className='lg:w-7/12'><img src={image} alt="Album" /></figure>
             <div className="card-body">
+                <figure className='lg:w-7/12'><img className='hidden' src={image} alt="Album" /></figure>
                 <h2 className="text-xl font-bold">{title}</h2>
                 <p>Mentor: <span className='text-primary'>{mentor}</span></p>
                 <p>Classes: <span className='font-bold'>{classes}</span></p>
@@ -18,8 +24,12 @@ const Course = () => {
                 <p>Course Price: <span className='font-bold'>{price}</span></p>
                 <p>Course Time: <span className='font-bold'>{time}</span></p>
                 <p>Launch Date: <span className='font-bold'>{date}</span></p>
+
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary w-full">Details</button>
+                    <ReactPrint trigger={() => <button className="btn btn-primary w-full">DOWNLOAD DETAILS</button>} content={() => ref.current} />
+                </div>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary w-full">GET PREMIUM</button>
                 </div>
             </div>
         </div>
